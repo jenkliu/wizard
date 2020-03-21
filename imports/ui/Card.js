759792c00738b0e4e2466cf352b944ea7239a24d
Jen <jenkliu@gmail.com>
2020-03-21 16:57:20 -0700

    add basic card implementation

---
+++ imports/ui/Card.js
@@ -0,0 +1,106 @@
+import React from "react";
+
+export class Deck extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  shuffleCards(deck) {
+    console.log("shuffling deck");
+    let counter = deck.length;
+    let t;
+    let i;
+
+    while (counter) {
+      i = Math.floor(Math.random() * counter--);
+      t = deck[counter];
+      deck[counter] = deck[i];
+      deck[i] = t;
+    }
+    return deck;
+  }
+
+  render() {
+    const suits = ["Spade", "Heart", "Club", "Diamond"];
+
+    const values = [
+      "A",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+    let cardDeck = [];
+    let card = [];
+
+    for (let x = 0; x < suits.length; x++) {
+      for (let y = 0; y < values.length; y++) {
+        card = { suit: suits[x], val: values[y] };
+        cardDeck.push(card);
+      }
+    }
+
+    this.shuffleCards(cardDeck);
+
+    return (
+      <div>
+        <button onClick={this.shuffleCards(cardDeck)}>Shuffle</button>
+        <div className="deck">
+          {cardDeck.map(function(card) {
+            return <Card suit={card.suit} value={card.val} />;
+          })}
+        </div>
+      </div>
+    );
+  }
+}
+
+const getSuitSymbol = suit => {
+  const suitMap = {
+    Spade: "♠︎",
+    Heart: "♥︎",
+    Club: "♣︎",
+    Diamond: "♦︎"
+  };
+  return suitMap[suit];
+};
+
+export const Card = props => {
+  if (props.suit == "Club" || props.suit == "Spade") {
+    return (
+      <div className="card card-black">
+        <div className="card-tl">
+          <div className="card-value">{props.value}</div>
+          <div className="card-suit">{getSuitSymbol(props.suit)}</div>
+        </div>
+        <div className="card-br">
+          <div className="card-value">{props.value}</div>
+          <div className="card-suit">{getSuitSymbol(props.suit)}</div>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="card card-red">
+        <div className="card-tl">
+          <div className="card-value">{props.value}</div>
+          <div className="card-suit">{getSuitSymbol(props.suit)}</div>
+        </div>
+        <div className="card-br">
+          <div className="card-value">{props.value}</div>
+          <div className="card-suit">{getSuitSymbol(props.suit)}</div>
+        </div>
+      </div>
+    );
+  }
+};
