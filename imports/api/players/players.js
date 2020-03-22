2bb2b0ee7f43fb0d3e8ceb25f9b791b6f90fddf0
Dean Yim <dean@loopsupport.com>
2020-03-21 19:25:37 -0700

    updated player model

--- imports/api/players/players.js
+++ imports/api/players/players.js
@@ -3,11 +3,20 @@ import { Mongo } from 'meteor/mongo';
 export const PlayersCollection = new Mongo.Collection('players');
 
 Meteor.methods({
-  'players.create'(name, browserKey){
-    player = PlayersCollection.insert({
-      name: name,
-      browserKey: browserKey,
-    });
-    console.log("Created a player: " + player);
+  'players.get_or_create'(name, browserKey, roomCode){
+    // If the browserKey x roomCode already exists, just grab that player.
+    player = PlayersCollection.find({browserKey: browserKey, roomCode: roomCode}).fetch()[0];
+    if (player == null) {
+      player = PlayersCollection.insert({
+        name: name,
+        browserKey: browserKey,
+        roomCode: roomCode,
+      });
+    }
+
+    // Refresh player object
+    player = PlayersCollection.find({browserKey: browserKey, roomCode: roomCode}).fetch()[0];
+    console.log("Player: " + player._id);
+    return player._id;
   },
 });
