7e33505673faf48fb69315acdd45011cd57cf46e
Jen <jenkliu@gmail.com>
2020-03-21 20:28:09 -0700

    add todo

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,23 +1,23 @@
-import React from "react";
-import { RoomsCollection } from "/imports/api/rooms/rooms";
-import { withTracker } from "meteor/react-meteor-data";
+import React from 'react';
+import { RoomsCollection } from '/imports/api/rooms/rooms';
+import { withTracker } from 'meteor/react-meteor-data';
 
 // these will all get converted to containers
-import WelcomeScreen from "./host/WelcomeScreen";
-import WaitingRoomScreen from "./host/WaitingRoomScreen";
+import WelcomeScreen from './host/WelcomeScreen';
+import WaitingRoomScreen from './host/WaitingRoomScreen';
 
-import BidScreen from "./host/BidScreen";
-import GameplayScreen from "./host/GameplayScreen";
+import BidScreen from './host/BidScreen';
+import GameplayScreen from './host/GameplayScreen';
 
 class App extends React.Component {
 	startGame = () => {
-		Meteor.call("rooms.start", this.props.room._id);
-		Meteor.call("rooms.rounds.start", this.props.room._id);
+		Meteor.call('rooms.start', this.props.room._id);
+		Meteor.call('rooms.rounds.start', this.props.room._id);
 	};
 
 	renderActiveGameScreen() {
 		switch (this.props.room.currRound.state) {
-			case "bid":
+			case 'bid':
 				// Dummy code
 				// return (
 				// 	<BidScreen
@@ -30,27 +30,21 @@ class App extends React.Component {
 				// 	/>
 				// );
 				return (
-					<BidScreen
-						playerIdToBids={this.props.room.currRound.playerIDsToBids}
-						players={this.props.room.players}
-					/>
+					<BidScreen playerIdToBids={this.props.room.currRound.playerIDsToBids} players={this.props.room.players} />
 				);
 				break;
-			case "play":
+			case 'play':
 				return (
+					// TODO: make gameplay screen show real data
 					<GameplayScreen
 						playerIdToBids={{ 1: 2, 2: 0, 3: 1 }}
-						players={[
-							{ _id: 1, name: "Jen" },
-							{ _id: 2, name: "Dean" },
-							{ _id: 3, name: "Max" }
-						]}
-						trumpCard={{ suit: "H", value: "5", type: "Standard" }}
+						players={[{ _id: 1, name: 'Jen' }, { _id: 2, name: 'Dean' }, { _id: 3, name: 'Max' }]}
+						trumpCard={{ suit: 'H', value: '5', type: 'Standard' }}
 						currTrick={{
 							cardsPlayed: {
-								1: { suit: "C", value: 10, type: "Standard" },
-								2: { suit: "D", value: 13, type: "Standard" },
-								3: { suit: null, value: null, type: "Jester" }
+								1: { suit: 'C', value: 10, type: 'Standard' },
+								2: { suit: 'D', value: 13, type: 'Standard' },
+								3: { suit: null, value: null, type: 'Jester' }
 							}
 						}}
 					/>
@@ -63,16 +57,12 @@ class App extends React.Component {
 
 	renderRoomScreen() {
 		switch (this.props.room.gameState) {
-			case "waiting":
+			case 'waiting':
 				return (
-					<WaitingRoomScreen
-						code={this.props.room.code}
-						players={this.props.room.players}
-						startGame={this.startGame}
-					/>
+					<WaitingRoomScreen code={this.props.room.code} players={this.props.room.players} startGame={this.startGame} />
 				);
 				break;
-			case "active":
+			case 'active':
 				return this.renderActiveGameScreen();
 				break;
 			default:
@@ -82,14 +72,12 @@ class App extends React.Component {
 
 	render() {
 		console.log(this.props.room);
-		return (
-			<div>{this.props.room ? this.renderRoomScreen() : <WelcomeScreen />}</div>
-		);
+		return <div>{this.props.room ? this.renderRoomScreen() : <WelcomeScreen />}</div>;
 	}
 }
 
 export default withTracker(() => {
-	Meteor.subscribe("rooms");
+	Meteor.subscribe('rooms');
 	const rooms = RoomsCollection.find().fetch();
 	return {
 		// TODO: properly fetch the correct room
