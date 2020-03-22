99915266efe7cb53fe85e78bf5238dbbd706ec25
Jen <jenkliu@gmail.com>
2020-03-21 20:26:35 -0700

    restore rooms.js

--- imports/api/rooms/rooms.js
+++ imports/api/rooms/rooms.js
@@ -1,148 +1,158 @@
-import { Mongo } from "meteor/mongo";
+import { Mongo } from 'meteor/mongo';
+import { PlayersCollection } from '/imports/api/players/players';
 
-export const RoomsCollection = new Mongo.Collection("rooms");
+export const RoomsCollection = new Mongo.Collection('rooms');
 
-var shuffle = require("shuffle-array");
+var shuffle = require('shuffle-array');
 
 Meteor.methods({
-	"rooms.create"() {
-		room = RoomsCollection.insert({
-			gameState: "waiting",
-			code: "BALLS",
-			createdAt: new Date(),
-			players: [
-				{
-					_id: 1,
-					name: "Dean"
-				},
-				{
-					_id: 2,
-					name: "Jennifer"
-				},
-				{
-					_id: 3,
-					name: "Bruno"
-				}
-			],
-			numTricksArr: [1, 2, 3, 4, 5],
-			rounds: [],
-			currRound: null
-		});
-		console.log("Made a room with ID: " + room);
-	},
-	"rooms.start"(roomID) {
-		room = RoomsCollection.find({ _id: roomID }).fetch()[0];
-		RoomsCollection.update(roomID, {
-			$set: { gameState: "active" }
-		});
-		console.log("Room is now active" + room);
-	},
+  'rooms.create'() {
+    room = RoomsCollection.insert({
+      gameState: 'waiting',
+      code: 'BALLS',
+      createdAt: new Date(),
+      players: [],
+      numTricksArr: [1, 2, 3, 4, 5],
+      rounds: [],
+      currRound: null
+    });
+    console.log('Made a room with ID: ' + room);
+  },
+  'rooms.addPlayer'(playerID) {
+    player = PlayersCollection.find({ _id: playerID }).fetch()[0];
+    room = RoomsCollection.find({ _id: player.roomID }).fetch()[0];
+    players = room.players;
+    players.push(player);
+    RoomsCollection.update(room._id, {
+      $set: { players: players }
+    });
+  },
+  'rooms.removePlayer'(playerID) {
+    player = PlayersCollection.find({ _id: playerID }).fetch()[0];
+    room = RoomsCollection.find({ _id: player.roomID }).fetch()[0];
+    players = room.players.filter(function(player) {
+      return player._id != playerID;
+    });
+    RoomsCollection.update(room._id, {
+      $set: { players: players }
+    });
+  },
+  'rooms.start'(roomID) {
+    room = RoomsCollection.find({ _id: roomID }).fetch()[0];
+    RoomsCollection.update(roomID, {
+      $set: { gameState: 'active', players: shuffle(room.players) }
+    });
+  },
 
-	"rooms.rounds.start"(roomID) {
-		room = RoomsCollection.find({ _id: roomID }).fetch()[0];
+  'rooms.rounds.start'(roomID) {
+    room = RoomsCollection.find({ _id: roomID }).fetch()[0];
 
-		// Update historical rounds array
-		rounds = room.rounds;
-		if (room.currRound) {
-			rounds.push(room.currRound);
-		}
+    // Update historical rounds array
+    rounds = room.rounds;
+    if (room.currRound) {
+      rounds.push(room.currRound);
+    }
+    roundsPlayed = rounds.length; // gratuitously self-documenting, lol
 
-		playerIDsToBids = room.players.reduce(function(map, obj) {
-			map[obj._id] = null;
-			return map;
-		}, {});
+    playerIDsToBids = room.players.reduce(function(map, obj) {
+      map[obj._id] = null;
+      return map;
+    }, {});
 
-		RoomsCollection.update(roomID, {
-			$set: {
-				rounds: rounds,
-				currRound: {
-					state: "bid",
-					activePlayer: null, // todo: set activePlayer
-					playerIDsToBids: playerIDsToBids,
-					numTricks: 5, // todo: set numTricks from numTricksArr
-					playerIDsToCards: {},
-					trumpCard: null,
-					tricks: [],
-					currTrick: null
-				}
-			}
-		});
-	},
-	"rooms.rounds.deal"(roomID) {
-		room = RoomsCollection.find({ _id: roomID }).fetch()[0];
+    // Action starts with the player 'after' the dealer.
+    activePlayer = room.players[(roundsPlayed + 1) % room.players.length];
 
-		deck = [];
-		suits = ["S", "H", "C", "D"];
-		suits.forEach(function(suit) {
-			for (value = 2; value <= 14; value++) {
-				deck.push({
-					suit: suit,
-					value: value,
-					type: "Standard"
-				});
-			}
-		});
-		shuffle(deck);
-		// Set trumpCard, ensuring (for now) that it's neither a Wizard nor a
-		// Jester.
-		trumpCard = deck.pop();
+    RoomsCollection.update(roomID, {
+      $set: {
+        rounds: rounds,
+        currRound: {
+          state: 'bid',
+          activePlayerID: activePlayer._id,
+          playerIDsToBids: playerIDsToBids,
+          numTricks: room.numTricksArr[roundsPlayed],
+          playerIDsToCards: {},
+          trumpCard: null,
+          tricks: [],
+          currTrick: null
+        }
+      }
+    });
+  },
+  'rooms.rounds.deal'(roomID) {
+    room = RoomsCollection.find({ _id: roomID }).fetch()[0];
 
-		// Add Wizards and Jesters
-		for (i = 0; i < 4; i++) {
-			deck.push({ suit: null, value: null, type: "Wizard" });
-			deck.push({ suit: null, value: null, type: "Jester" });
-		}
-		shuffle(deck);
+    deck = [];
+    suits = ['S', 'H', 'C', 'D'];
+    suits.forEach(function(suit) {
+      for (value = 2; value <= 14; value++) {
+        deck.push({
+          suit: suit,
+          value: value,
+          type: 'Standard'
+        });
+      }
+    });
+    shuffle(deck);
+    // Set trumpCard, ensuring (for now) that it's neither a Wizard nor a
+    // Jester.
+    trumpCard = deck.pop();
 
-		// Deal out
-		playerIDsToCards = {};
-		room.players.forEach(function(player) {
-			hand = [];
-			for (i = 0; i < room.currRound.numTricks; i++) {
-				hand.push(deck.pop());
-			}
-			playerIDsToCards[player._id] = hand;
-		});
+    // Add Wizards and Jesters
+    for (i = 0; i < 4; i++) {
+      deck.push({ suit: null, value: null, type: 'Wizard' });
+      deck.push({ suit: null, value: null, type: 'Jester' });
+    }
+    shuffle(deck);
 
-		currRound = room.currRound;
-		currRound.trumpCard = trumpCard;
-		currRound.playerIDsToCards = playerIDsToCards;
-		RoomsCollection.update(roomID, {
-			$set: { currRound: currRound }
-		});
-	},
-	"rooms.rounds.updateBid"(roomID, playerID, bid) {
-		room = RoomsCollection.find({ _id: roomID }).fetch()[0];
-		currRound = room.currRound;
-		currRound.playerIDsToBids[playerID] = bid;
-		RoomsCollection.update(roomID, {
-			$set: {
-				currRound: currRound
-				// todo: update activePlayer to the next one
-			}
-		});
-	},
-	"rooms.rounds.beginPlay"(roomID) {
-		// todo: change round state from "bid" to "play"
-	},
-	"rooms.rounds.playerIDsToTricksWon"(round) {
-		// todo: helper function to determine which tricks were won by which players
-	},
-	"rooms.rounds.playerIDsToScores"(round) {
-		// todo: helper function to determine the scores of each round
-	},
+    // Deal out
+    playerIDsToCards = {};
+    room.players.forEach(function(player) {
+      hand = [];
+      for (i = 0; i < room.currRound.numTricks; i++) {
+        hand.push(deck.pop());
+      }
+      playerIDsToCards[player._id] = hand;
+    });
 
-	"rooms.tricks.start"(roomID) {
-		// todo: move currTrick to the tricks array
-		// todo: set currTrick
-		currTrick = {
-			leadPlayerID: "",
-			winningPlayerID: "",
-			playerIDsToCards: {},
-			leadCard: ""
-		};
-	},
-	"rooms.tricks.playCard"(roomID, playerID, card) {
-		// todo:
-	}
+    currRound = room.currRound;
+    currRound.trumpCard = trumpCard;
+    currRound.playerIDsToCards = playerIDsToCards;
+    RoomsCollection.update(roomID, {
+      $set: { currRound: currRound }
+    });
+  },
+  'rooms.rounds.updateBid'(roomID, playerID, bid) {
+    room = RoomsCollection.find({ _id: roomID }).fetch()[0];
+    currRound = room.currRound;
+    currRound.playerIDsToBids[playerID] = bid;
+    RoomsCollection.update(roomID, {
+      $set: {
+        currRound: currRound
+        // todo: update activePlayer to the next one
+      }
+    });
+  },
+  'rooms.rounds.beginPlay'(roomID) {
+    // todo: change round state from "bid" to "play"
+  },
+  'rooms.rounds.playerIDsToTricksWon'(round) {
+    // todo: helper function to determine which tricks were won by which players
+  },
+  'rooms.rounds.playerIDsToScores'(round) {
+    // todo: helper function to determine the scores of each round
+  },
+
+  'rooms.tricks.start'(roomID) {
+    // todo: move currTrick to the tricks array
+    // todo: set currTrick
+    currTrick = {
+      leadPlayerID: '',
+      winningPlayerID: '',
+      playerIDsToCards: {},
+      leadCard: ''
+    };
+  },
+  'rooms.tricks.playCard'(roomID, playerID, card) {
+    // todo:
+  }
 });
