c7e0503e2c116c521a06449a78d4e64b11a6c1e9
Jen <jenkliu@gmail.com>
2020-03-21 17:35:36 -0700

    cleanup Card component

--- imports/ui/Card.js
+++ imports/ui/Card.js
@@ -1,4 +1,6 @@
 import React from "react";
+import classNames from "classnames";
+import PropTypes from "prop-types";
 
 const getSuitSymbol = suit => {
   const suitMap = {
@@ -11,32 +13,29 @@ const getSuitSymbol = suit => {
 };
 
 // TODO: support wizards and jesters
-export const Card = props => {
-  if (props.suit == "C" || props.suit == "S") {
+export default class Card extends React.Component {
+  render() {
+    const { suit, value } = this.props;
+    const classes = classNames("card", {
+      "card-black": suit == "C" || suit == "S",
+      "card-red": suit == "H" || suit == "D"
+    });
     return (
-      <div className="card card-black">
+      <div className={classes}>
         <div className="card-tl">
-          <div className="card-value">{props.value}</div>
-          <div className="card-suit">{getSuitSymbol(props.suit)}</div>
+          <div className="card-value">{value}</div>
+          <div className="card-suit">{getSuitSymbol(suit)}</div>
         </div>
         <div className="card-br">
-          <div className="card-value">{props.value}</div>
-          <div className="card-suit">{getSuitSymbol(props.suit)}</div>
-        </div>
-      </div>
-    );
-  } else {
-    return (
-      <div className="card card-red">
-        <div className="card-tl">
-          <div className="card-value">{props.value}</div>
-          <div className="card-suit">{getSuitSymbol(props.suit)}</div>
-        </div>
-        <div className="card-br">
-          <div className="card-value">{props.value}</div>
-          <div className="card-suit">{getSuitSymbol(props.suit)}</div>
+          <div className="card-value">{value}</div>
+          <div className="card-suit">{getSuitSymbol(suit)}</div>
         </div>
       </div>
     );
   }
+}
+
+Card.propTypes = {
+  suit: PropTypes.string, // "S", "C", "H", "D"
+  value: PropTypes.string
 };
--- imports/ui/host/GameplayScreen.js
+++ imports/ui/host/GameplayScreen.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Card } from "../Card";
+import Card from "../Card";
 
 export default class Gameplay extends React.Component {
   renderPlayer(player) {
