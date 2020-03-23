d442e5903b7d2a83768eb76e554486e3f5588813
Jen <jenkliu@gmail.com>
2020-03-22 22:19:45 -0700

    show who won the trick before moving on to the next

--- imports/ui/host/GameplayScreen.js
+++ imports/ui/host/GameplayScreen.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Card from '../Card';
+import classNames from 'classnames';
 
 export default class GameplayScreen extends React.Component {
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    // someone just won the trick, show who won then start the next trick
+    if (
+      !(prevProps.currTrick && prevProps.currTrick.winningPlayerID) &&
+      (this.props.currTrick && this.props.currTrick.winningPlayerID)
+    ) {
+      setTimeout(() => {
+        this.props.startTrick();
+      }, 2000);
+    }
+  }
+
   renderPlayer(player) {
     const cardPlayed = this.props.currTrick && this.props.currTrick.playerIDsToCards[player._id];
+    const cardClasses = classNames('player-card', {
+      'is-winner': this.props.currTrick && this.props.currTrick.winningPlayerID === player._id
+    });
 
     return (
       <div className="player-card-container" key={player._id}>
-        <div className="player-card">
+        <div className={cardClasses}>
           {cardPlayed ? (
             <Card value={cardPlayed.value} suit={cardPlayed.suit} type={cardPlayed.type} />
           ) : (
@@ -40,5 +56,6 @@ GameplayScreen.propTypes = {
   playerIdToBids: PropTypes.object.isRequired, // { player_id: bid}
   players: PropTypes.array.isRequired,
   trumpCard: PropTypes.object.isRequired, // card: { suit: 'H'.isRequired, value: '4', type: 'Standard'}
-  currTrick: PropTypes.object.isRequired // {cardsPlayed: {playerId: card}}
+  currTrick: PropTypes.object.isRequired, // {cardsPlayed: {playerId: card}}
+  startTrick: PropTypes.func.isRequired
 };
